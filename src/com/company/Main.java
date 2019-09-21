bfc30a69605afef167731999c8e66f68886bab1c
Lukas-github <lukskup@gmail.com>
2019-09-21 21:34:38 +0200

    Pierwszy commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x=1;
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(x+". "+"No to lecimy z tym koksem");
+            x++;
+            if (x==9){
+                System.out.println(" KOTY MAJĄ DZIEWIĘĆ ŻYĆ");
+            }
+        }
+
+    }
+}
